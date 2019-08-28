7c41e0f99e51f224c0358aa6db27a8b6042f57ae
Dmitry Baklanov <n79613928275@gmail.com>
2019-08-28 17:14:20 +0400

    Read input catalog for xml file

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <?php 
-$filename = "./GIVED_SOC_CARD_20190828120119.xml";
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-    if (file_exists($filename)) {
-        $xml = simplexml_load_file($filename);
-        
-        for ($i=0;$i<count($xml);$i++){
-            echo $xml->SocCard[$i]->CARD_NUM.'<br/>';
-            echo $xml->SocCard[$i]->CARD_SER.'<br/>';
-            echo $xml->SocCard[$i]->SNR.'<br/>';
-            echo $xml->SocCard[$i]->DT_CARD.'<br/>';
-            echo $xml->SocCard[$i]->SocCardCategory->CODE_L.'<br/>';
+$catalog_in = "./input";
+$catalog_out = "./out";
+//Convert PAN num for necessary view
+function createPAN($obj){
+    $pan =  substr($obj->CARD_NUM,0,6).' '.substr($obj->CARD_NUM,6,2).' '.substr($obj->CARD_NUM,8,10).' '.substr($obj->CARD_NUM,18,1).' '.substr($obj->CARD_SER,2,2).'/'.substr($obj->CARD_SER,0,2).' '.substr($obj->CARD_SER,4,4);
+    return $pan;
+}
+//Convert CHIP num for necessary view
+function createCHIP($obj){
+    $chipnum = substr($obj->SNR,6,2).substr($obj->SNR,4,2).substr($obj->SNR,2,2).substr($obj->SNR,0,2).'000000000000';
+    return $chipnum;
+}
+if ($handle = opendir($catalog_in)){
+    while (false !== ($file = readdir($handle))){
+        if ($file !== "." && $file !== "..") {
+            $xml = simplexml_load_file($catalog_in.'/'.$file);
+            for ($i=0;$i<count($xml);$i++){
+                $pan = createPAN($xml->SocCard[$i]);
+                $chipnum = createCHIP($xml->SocCard[$i]);
+                echo 'SocialCard - '.$pan.'<br />';
+                echo 'Chip - '.$chipnum.'<br />';
+                echo $xml->SocCard[$i]->CARD_SER.'<br />';
+                echo $xml->SocCard[$i]->SNR.'<br />';
+                echo $xml->SocCard[$i]->DT_CARD.'<br />';
+                echo $xml->SocCard[$i]->SocCardCategory->CODE_L.'<br />';
+                    
+                };
+             }
             
-        };
+        }
+
     } else {
-        exit('Не удалось открыть файл {$filename}');
+        echo "Каталог ввода не найден";
     }
     
-    
     ?>
-</body>
-
